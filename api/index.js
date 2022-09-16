93bf312ce987bae80b875b4d6bbc5493d1acb38e
Courtney Harris <cjharris1985@gmail.com>
2022-09-15 19:18:10 -0500

    added back backend api for index, dogs, users & utils.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,59 @@
-const apiRouter = require('express').Router();
+const express = require('express');
+const apiRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
-apiRouter.get('/', (req, res, next) => {
-  res.send({
-    message: 'API is under construction!',
-  });
+const { getUserById } = require('../../db');
+const { JWT_SECRET } = process.env;
+
+apiRouter.use(async (req, res, next) => {
+   const prefix = 'Bearer ';
+   const auth = req.header('Authorization');
+
+   if (!auth) {
+      next();
+   } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+
+      try {
+         const { id } = jwt.verify(token, JWT_SECRET);
+
+         if (id) {
+            req.user = await getUserById(id);
+            next();
+         }
+      } catch ({ name, message }) {
+         next({ name, message });
+      }
+   } else {
+      next({
+         name: 'AuthorizationHeaderError',
+         message: `Authorization token must start with ${prefix}`,
+      });
+   }
 });
 
-apiRouter.get('/health', (req, res, next) => {
-  res.send({
-    healthy: true,
-  });
+apiRouter.use((req, res, next) => {
+   if (req.user) {
+      console.log('User is set', req.user);
+   }
+
+   next();
 });
 
-// place your routers here
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
+
+const dogsRouter = require('./dogs');
+apiRouter.use('/dogs', dogsRouter);
+
+const ordersRouter = require('./orders');
+apiRouter.use('/orders', ordersRouter);
+
+apiRouter.use((error, req, res, next) => {
+   res.send({
+      name: error.name,
+      message: error.message,
+   });
+});
 
 module.exports = apiRouter;
