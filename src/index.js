5f929e8413bf9dbe8015dc1623de191331649fc2
Noelke10 <Noelke10@gmail.com>
2022-09-17 11:50:54 -0500

    started routes in app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './components';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { App } from "./components";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import './style/index.css';
+import "./style/index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
