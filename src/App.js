a492a36cbb8ab8ed96298eea7b7c19ce4b0bc15a
Noelke10 <Noelke10@gmail.com>
2022-09-22 19:44:05 -0500

    cart rendering

--- src/App.js
+++ src/App.js
@@ -1,92 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import {
-   Navbar,
-   Home,
-   Login,
-   Register,
-   Products,
-} from './components';
-import {
-   getUser,
-   fetchAllproducts,
-   getUsersCart,
-} from './frontend-api';
+import React, { useState, useEffect } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import { Navbar, Home, Login, Register, Products, Cart } from "./components";
+import { getUser, fetchAllproducts, getUsersCart } from "./frontend-api";
 // import { getAPIHealth } from './frontend-api/index';
 
 const App = () => {
-   const [APIHealth, setAPIHealth] = useState('');
-   const [token, setToken] = useState(
-      localStorage.getItem('token')
-         ? localStorage.getItem('token')
-         : ''
-   );
-   const [username, setUsername] = useState(
-      localStorage.getItem('username')
-         ? localStorage.getItem('username')
-         : ''
-   );
-   const [userData, setUserData] = useState({});
-   const [password, setPassword] = useState();
-   const [order, setOrder] = useState();
-   const [products, setproducts] = useState([]);
-   const [cart, setCart] = useState([]);
+  const [APIHealth, setAPIHealth] = useState("");
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
+  const [username, setUsername] = useState(
+    localStorage.getItem("username") ? localStorage.getItem("username") : ""
+  );
+  const [userData, setUserData] = useState({});
+  const [password, setPassword] = useState();
+  const [order, setOrder] = useState();
+  const [products, setproducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
-   //    useEffect(() => {
-   //       const getAPIStatus = async () => {
-   //          const { healthy } = await getAPIHealth();
-   //          setAPIHealth(healthy ? 'api is up! :D' : 'api is down :/');
-   //       };
+  //    useEffect(() => {
+  //       const getAPIStatus = async () => {
+  //          const { healthy } = await getAPIHealth();
+  //          setAPIHealth(healthy ? 'api is up! :D' : 'api is down :/');
+  //       };
 
-   //       getAPIStatus();
-   //    }, []);
+  //       getAPIStatus();
+  //    }, []);
 
-   useEffect(async () => {
-      if (!token) {
-         setToken(localStorage.getItem('capstone-token'));
-         return;
-      }
-      const data = await getUser(token);
-      setUserData(data);
-   }, [token]);
+  useEffect(async () => {
+    if (!token) {
+      setToken(localStorage.getItem("capstone-token"));
+      return;
+    }
+    const data = await getUser(token);
+    setUserData(data);
+  }, [token]);
 
-   // products
-   useEffect(() => {
-      const getAllproducts = async () => {
-         const result = await fetchAllproducts();
-         console.log(result);
-         setproducts(result);
-         // setproductsToDisplay(result.data.products);
-      };
-      getAllproducts().catch(console.error);
-   }, [setproducts]);
+  // products
+  useEffect(() => {
+    const getAllproducts = async () => {
+      const result = await fetchAllproducts();
+      console.log(result);
+      setproducts(result);
+      // setproductsToDisplay(result.data.products);
+    };
+    getAllproducts().catch(console.error);
+  }, [setproducts]);
 
-   // useEffect(async () => {
-   // 	setOrder([]);
-   // 	if (userData.id !== undefined) {
-   // 		const usersCart = await getUsersCart(userData.id, token);
-   // 		if (typeof usersCart === 'object') {
-   // 			setCart(usersCart);
-   // 		}
-   // 	} else {
-   // 		let localCart = JSON.parse(localStorage.getItem('capstone-cart'));
-   // 		if (!localCart) {
-   // 			localCart = [];
-   // 			localStorage.setItem(
-   // 				'capstone-cart',
-   // 				JSON.stringify(localCart)
-   // 			);
-   // 		}
-   // 		setCart(localCart);
-   // 	}
-   // }, [userData]);
+  // useEffect(async () => {
+  // 	setOrder([]);
+  // 	if (userData.id !== undefined) {
+  // 		const usersCart = await getUsersCart(userData.id, token);
+  // 		if (typeof usersCart === 'object') {
+  // 			setCart(usersCart);
+  // 		}
+  // 	} else {
+  // 		let localCart = JSON.parse(localStorage.getItem('capstone-cart'));
+  // 		if (!localCart) {
+  // 			localCart = [];
+  // 			localStorage.setItem(
+  // 				'capstone-cart',
+  // 				JSON.stringify(localCart)
+  // 			);
+  // 		}
+  // 		setCart(localCart);
+  // 	}
+  // }, [userData]);
 
-   return (
-      <div className='container'>
-         <main>
-            <div className='title'>Tatooine Rescue</div>
-            <Navbar token={token} />
-            {/* <nav>
+  return (
+    <div className="container">
+      <main>
+        <div className="title">Tatooine Rescue</div>
+        <Navbar token={token} />
+        {/* <nav>
                <Link className='tab' to='/home'>
                   Home
                </Link>
@@ -102,8 +88,8 @@ const App = () => {
 
             </nav> */}
 
-            <Switch>
-               {/* <Route
+        <Switch>
+          {/* <Route
                   exact
                   path='/'
                   element={
@@ -114,36 +100,35 @@ const App = () => {
                      />
                   }
                /> */}
-               <Route path='/home'>
-                  <Home
-                     token={token}
-                     setproducts={setproducts}
-                     products={products}
-                  />
-               </Route>
-               <Route path='/login'>
-                  <Login
-                     token={token}
-                     setToken={setToken}
-                     username={username}
-                     setUsername={setUsername}
-                  />
-               </Route>
-               <Route path='/register'>
-                  <Register
-                     token={token}
-                     setToken={setToken}
-                     username={username}
-                     setUsername={setUsername}
-                  />
-               </Route>
-               <Route path='/products' element={<Products />} />
-               {/* <Route path="/cart/:user" element={<Cart />} />
+          <Route path="/home">
+            <Home token={token} setproducts={setproducts} products={products} />
+          </Route>
+          <Route path="/login">
+            <Login
+              token={token}
+              setToken={setToken}
+              username={username}
+              setUsername={setUsername}
+            />
+          </Route>
+          <Route path="/register">
+            <Register
+              token={token}
+              setToken={setToken}
+              username={username}
+              setUsername={setUsername}
+            />
+          </Route>
+          <Route path="/cart">
+            <Cart token={token} />
+          </Route>
+          <Route path="/products" element={<Products />} />
+          {/* <Route path="/cart/:user" element={<Cart />} />
 					<Route path="/cart/:userId" element={<Cart />} /> */}
-            </Switch>
-         </main>
-      </div>
-   );
+        </Switch>
+      </main>
+    </div>
+  );
 };
 
 export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 // import { getproductsByBreed } from "../../db";
 
-export { default as Navbar } from './Navbar';
-export { default as Login } from './Login';
-export { default as Logout } from './Logout';
-export { default as Register } from './Register';
-export { default as Products } from './Products';
-// export { default as Cart } from "./Cart";
-export { default as Home } from './Home';
+export { default as Navbar } from "./Navbar";
+export { default as Login } from "./Login";
+export { default as Logout } from "./Logout";
+export { default as Register } from "./Register";
+export { default as Products } from "./Products";
+export { default as Cart } from "./Cart";
+export { default as Home } from "./Home";
