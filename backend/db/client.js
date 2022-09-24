058221f2d6e099fce082bbc408a6c8aebfb9821e
MsSwindle <msmaryswindle@yahoo.com>
2022-09-24 11:58:37 -0500

    changed db

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const connectionString =
-	process.env.DATABASE_URL || 'https://localhost:5432/tatooine_rescue-dev';
+	process.env.DATABASE_URL || 'https://localhost:5432/tatooine_rescue2';
 
 const client = new Pool({
 	connectionString,
