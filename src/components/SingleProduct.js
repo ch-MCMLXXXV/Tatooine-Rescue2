9be79d637cd3c11f67dce280c66916da476aba86
Courtney Harris <cjharris1985@gmail.com>
2022-09-24 12:16:18 -0500

    home.js updated

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,55 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchProductById } from '../index';
+import { fetchProductById } from '../frontend-api/index';
 
 const SingleProduct = ({ _productsId, inCart, cartDetails }) => {
-	const [singleProduct, setSingleProduct] = useState({});
-	const { productsId } = useParams();
+   const [singleProduct, setSingleProduct] = useState({});
+   const { productsId } = useParams();
 
-	useEffect(() => {
-		const fetchdata = async () => {
-			try {
-				const data = await fetchProductById(productsId || _productsId);
-				console.log('data', data);
-				setSingleProduct(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchdata();
-	}, []);
+   useEffect(() => {
+      const fetchdata = async () => {
+         try {
+            const data = await fetchProductById(
+               productsId || _productsId
+            );
+            console.log('data', data);
+            setSingleProduct(data);
+         } catch (error) {
+            console.log(error);
+         }
+      };
+      fetchdata();
+   }, []);
 
-	return (
-		<>
-			<div className="singleProduct">
-				<h3
-					className={`product-name product-color${
-						((productsId - 1) % 3) + 1
-					}`}
-				>
-					{singleProduct.name}
-				</h3>
-				<div>
-					<img
-						src={`${singleProduct.imageURL}`}
-						className="imageMedium"
-						alt="dog"
-					/>
-				</div>
-				<div>{singleProduct.description}</div>
-				<div>
-					<div>${singleProduct.price}</div>
-				</div>
-				<div>{singleProduct.inStock ? null : 'OUT OF STOCK'}</div>
-				<div>{singleProduct.category}</div>
-			</div>
-			{inCart ? (
-				<span>{cartDetails.price}</span>
-			) : (
-				<button>Add to cart</button>
-			)}
-		</>
-	);
+   return (
+      <>
+         <div className='singleProduct'>
+            <h3
+               className={`product-name product-color${
+                  ((productsId - 1) % 3) + 1
+               }`}>
+               {singleProduct.name}
+            </h3>
+            <div>
+               <img
+                  src={`${singleProduct.imageURL}`}
+                  className='imageMedium'
+                  alt='dog'
+               />
+            </div>
+            <div>{singleProduct.description}</div>
+            <div>
+               <div>${singleProduct.price}</div>
+            </div>
+            <div>{singleProduct.inStock ? null : 'OUT OF STOCK'}</div>
+            <div>{singleProduct.category}</div>
+         </div>
+         {inCart ? (
+            <span>{cartDetails.price}</span>
+         ) : (
+            <button>Add to cart</button>
+         )}
+      </>
+   );
 };
 
 export default SingleProduct;
